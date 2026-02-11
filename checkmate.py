d5a4afc992e5f1ccf86cb648a6c4e28f8309c592
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2026-02-11 20:46:46 +0700

    feat(checkmate): implement board parsing logic
    Resolves #2

--- checkmate.py
+++ checkmate.py
@@ -1,3 +1,15 @@
 def checkmate(board: str):
-    # temporary placeholder
-    print("Fail")
+    parsed = parse_board(board)
+    return parsed
+
+
+def parse_board(board_str: str) -> list | None:
+    if not isinstance(board_str, str):
+        return None
+
+    rows = board_str.splitlines()
+    rows = [row for row in rows if row.strip() != ""]  # Remove empty lines
+
+    # 2D list
+    board = [list(row) for row in rows]
+    return board
